eb18d825c1a335a45944be3c589e7e4942423d73
guillaume71 <guillaume271293@hotmail.fr>
2023-04-12 16:17:57 +0200

    Projet

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserSerializer
 
@@ -6,4 +7,4 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = []
+    permission_classes = [AllowAny]
